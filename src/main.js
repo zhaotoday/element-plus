7119ae22c609ff344fb79f5d99553ddc377711ed
zhaotoday <729234283@qq.com>
2017-04-07 22:54:45 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import '@/utils/init'
 
 Vue.config.productionTip = false
 
@@ -11,5 +12,5 @@ new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: {App}
 })
