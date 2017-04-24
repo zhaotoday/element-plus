1b505e98120ac05c5d13862879b3e7d6fab37402
zhaotoday <729234283@qq.com>
2017-04-24 20:57:38 +0800

    update

---
+++ backup/Articles/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>articles</p>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import './i18n'
+
+  export default {
+    name: 'article-entry'
+  }
+</script>
