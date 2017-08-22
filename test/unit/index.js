a4841053109743b1014672c262a3ef38ab9aca22
820514 <820514@nd>
2017-08-22 17:51:42 +0800

    vue-cli 升级到最新版本

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
-Vue.config.productionTip = false
 
-// Polyfill fn.bind() for PhantomJS
-/* eslint-disable no-extend-native */
-Function.prototype.bind = require('function-bind')
+Vue.config.productionTip = false
 
 // require all test files (files that ends with .spec.js)
 const testsContext = require.context('./specs', true, /\.spec$/)
