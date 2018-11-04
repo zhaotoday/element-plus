8bd0af36786e6bd93caf7c8ce0993fd760493a66
zhaojintian <729234283@qq.com>
2018-11-04 15:09:01 +0800

    优化

--- src/utils/consts/menus.js
+++ src/utils/consts/menus.js
@@ -2,55 +2,11 @@ export default [
   {
     icon: 'android-desktop',
     name: 'website',
-    title: '测试类目管理',
+    title: '一级菜单',
     children: [
       {
-        title: '测试类目列表',
-        route: '/website/news/news/articles/categories'
-      }
-    ]
-  },
-  {
-    icon: 'android-desktop',
-    name: 'website',
-    title: '测试题管理',
-    children: [
-      {
-        title: '测试题列表',
-        route: '/website/news/news/articles/categories'
-      }
-    ]
-  },
-  {
-    icon: 'android-desktop',
-    name: 'website',
-    title: '用户管理',
-    children: [
-      {
-        title: '用户详情',
-        route: '/website/news/news/articles/categories'
-      }
-    ]
-  },
-  {
-    icon: 'android-desktop',
-    name: 'website',
-    title: '订单管理',
-    children: [
-      {
-        title: '订单列表',
-        route: '/website/news/news/articles/categories'
-      }
-    ]
-  },
-  {
-    icon: 'android-desktop',
-    name: 'website',
-    title: '财务管理',
-    children: [
-      {
-        title: '财务管理',
-        route: '/website/news/news/articles/categories'
+        title: '二级菜单',
+        route: '/some/route'
       }
     ]
   }
