1267fac984f02be651f6bfedc827a085e94533bc
zhaojintian <729234283@qq.com>
2018-11-18 16:38:31 +0800

    update

--- src/mixins/global.js
+++ src/mixins/global.js
@@ -2,11 +2,11 @@ import store from '../store'
 import { mapActions } from 'vuex'
 
 export default {
-  methods: mapActions({
-    resetState: 'resetState'
-  }),
   beforeRouteEnter (to, from, next) {
     store.dispatch('resetState', {})
     next()
-  }
+  },
+  methods: mapActions({
+    resetState: 'resetState'
+  })
 }
