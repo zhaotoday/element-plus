16285ef06f70fe49a5f55cee05a9a0f559dec174
zhaojintian <729234283@qq.com>
2018-11-18 16:44:49 +0800

    统一命名规范

--- src/utils/styles.js
+++ src/utils/styles.js
@@ -1,5 +1,6 @@
 import '../styles/global/classes/colors.scss'
 import '../styles/global/classes/fonts.scss'
+import '../styles/global/classes/iconfont.scss'
 import '../styles/global/components/example.scss'
 import '../styles/global/objects/grid.scss'
 import '../styles/global/objects/media.scss'
