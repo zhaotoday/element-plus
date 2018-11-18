b9104f850a09fe86915dea47ac3245aeee3e9163
zhaojintian <729234283@qq.com>
2018-11-18 16:32:59 +0800

    优化

--- src/utils/helpers/base.js
+++ src/utils/helpers/base.js
@@ -3,6 +3,15 @@ import helpers from 'jt-helpers'
 
 export default {
   ...helpers,
+  goBack () {
+    window.history.go(-1)
+  },
+  open (url) {
+    window.open(url)
+  },
+  deepCopy (obj) {
+    return JSON.parse(JSON.stringify(obj))
+  },
   getFileURLById (id) {
     return `${consts.BASE_URL}/apis/v1/files/${id}`
   },
