5818e6ad56c99fad313d53c294e1e509768c3c2d
zhaojintian <729234283@qq.com>
2018-11-18 11:12:47 +0800

    update

--- src/components/List/index.js
+++
@@ -1,8 +0,0 @@
-import CList from './components/List'
-import CListHeader from './components/Header'
-import CListOperations from './components/Operations'
-import CListSearch from './components/Search'
-import CListNavigation from './components/Navigation'
-
-export { CListHeader, CListOperations, CListSearch, CListNavigation }
-export default CList
