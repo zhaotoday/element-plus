96d987f1996554d73f4192e7ab3e8d87096b4f53
zhaojintian <729234283@qq.com>
2019-04-25 11:53:20 +0800

    优化

--- src/utils/consts/sidebar-menus.js
+++ src/utils/consts/sidebar-menus.js
@@ -1,61 +1,16 @@
 export default [
   {
-    icon: 'ios-cart',
-    name: 'products',
-    title: '商品',
+    icon: 'logo-buffer',
+    name: 'surveys',
+    title: '问卷调查',
     children: [
       {
-        title: '商品',
-        route: '/products/products/index'
+        title: '主题',
+        route: '/surveys/surveys/subjects'
       },
       {
-        title: '商品分类',
-        route: '/products/products/categories'
-      },
-      {
-        title: '商品评价',
-        route: '/products/comments/index'
-      }
-    ]
-  },
-  {
-    icon: 'md-pricetag',
-    name: 'orders',
-    title: '订单',
-    children: [
-      {
-        title: '订单',
-        route: '/orders/orders/index'
-      }
-    ]
-  },
-  {
-    icon: 'md-help',
-    name: 'helpers',
-    title: '帮助',
-    children: [
-      {
-        title: '帮助',
-        route: '/helpers/helpers/index'
-      },
-      {
-        title: '帮助分类',
-        route: '/helpers/helpers/categories'
-      }
-    ]
-  },
-  {
-    icon: 'ios-map',
-    name: 'marketing',
-    title: '营销',
-    children: [
-      {
-        title: '广告',
-        route: '/marketing/ads/index'
-      },
-      {
-        title: '优惠券',
-        route: '/marketing/coupons/index'
+        title: '评价',
+        route: '/surveys/surveys/results'
       }
     ]
   },
@@ -67,47 +22,6 @@ export default [
       {
         title: '用户',
         route: '/wxUsers/wxUsers/index'
-      }
-    ]
-  },
-  {
-    icon: 'md-key',
-    name: 'rbac',
-    title: '角色权限',
-    children: [
-      {
-        title: '角色',
-        route: '/rbac/rbac/roles'
-      },
-      {
-        title: '资源',
-        route: '/rbac/rbac/resources'
-      }
-    ]
-  },
-  {
-    icon: 'logo-buffer',
-    name: 'surveys',
-    title: '问卷调查',
-    children: [
-      {
-        title: '主题',
-        route: '/surveys/surveys/subjects'
-      },
-      {
-        title: '结果',
-        route: '/surveys/surveys/results'
-      }
-    ]
-  },
-  {
-    icon: 'ios-print',
-    name: 'logs',
-    title: '日志',
-    children: [
-      {
-        title: '日志',
-        route: '/logs/logs/index'
       }
     ]
   }
