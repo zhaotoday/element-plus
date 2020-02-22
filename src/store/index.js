e85ff9897794df6f6bbf491b451a78639b56de2f
zhaojintian <729234283@qq.com>
2020-02-23 01:20:30 +0800

    update

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
+import storage from "jt-storage";
 import getters from "./getters";
 import actions from "./actions";
 import mutations from "./mutations";
@@ -7,6 +9,16 @@ import mutations from "./mutations";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  plugins: [
+    createPersistedState({
+      paths: ["public/dicts"],
+      storage: {
+        getItem: key => storage.get(key),
+        setItem: (key, value) => storage.set(key, value),
+        removeItem: key => storage.remove(key)
+      }
+    })
+  ],
   state: {},
   getters,
   actions,
