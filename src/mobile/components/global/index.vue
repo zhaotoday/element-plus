8f91d20afe08c3435ae7866fd3ee054fb534f526
zhao <729234283@qq.com>
2020-06-29 00:41:09 +0800

    update

--- src/mobile/components/global/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="c-global bgc32 fs28 u-tac">{{ name }}</div>
-</template>
-
-<script src="./script.js"></script>
-
-<style lang="scss" src="./style.scss"></style>
--- src/mobile/components/rich-text/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="c-rich-text bgc11" v-html="content"></div>
-</template>
-
-<script src="./script.js"></script>
-
-<style lang="scss" src="./style.scss"></style>
