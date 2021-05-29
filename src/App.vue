cdf3d338767476525bd41986f031293092483f17
zhaotoday <729234283@qq.com>
2021-05-29 16:55:16 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,3 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <router-view />
 </template>
-
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import "@/assets/styles/global/index.scss";
+import ElementPlus from "element-plus";
+import locale from "element-plus/lib/locale/lang/zh-cn";
+import "element-plus/lib/theme-chalk/index.css";
+import "@/utils/polyfills";
+import { config } from "@/utils/config";
+import { useComponents } from "@/utils/use-components";
+import App from "./App.vue";
+import { store } from "./store";
+import { router } from "./router";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app
+  .use(store)
+  .use(router)
+  .use(ElementPlus, { size: "medium", locale })
+  .mount("#app");
+
+config(app);
+useComponents(app);
