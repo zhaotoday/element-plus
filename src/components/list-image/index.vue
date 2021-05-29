510ba4297f234fe5da44aa6385ddb3a135e9f563
zhaotoday <729234283@qq.com>
2021-05-29 16:18:11 +0800

    update

---
+++ src/components/list-image/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <el-image
+    :style="{ display: 'block', cursor: 'pointer', width, height }"
+    :src="src"
+    fit="cover"
+    :preview-src-list="[src]"
+    title="查看大图"
+  />
+</template>
+
+<script src="./script.js"></script>
---
+++ src/components/list-image/script.js
@@ -0,0 +1,14 @@
+export default {
+  name: "ListImage",
+  props: {
+    src: String,
+    width: {
+      type: String,
+      default: "100px"
+    },
+    height: {
+      type: String,
+      default: "100px"
+    }
+  }
+};
