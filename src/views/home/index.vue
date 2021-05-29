5be1c22632465ca604f86bd95961cdc1d545dd1f
zhaotoday <729234283@qq.com>
2021-05-29 21:10:07 +0800

    update

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,3 @@
 <template>
-  <div class="v-home">
-    <img
-      v-for="item in 4"
-      :key="item"
-      class="b-image"
-      :src="`${$consts.BaseUrl}images/pages/home/${item}.jpg`"
-    />
-  </div>
+  <div class="v-home">home</div>
 </template>
-
-<style lang="scss" src="./style.scss"></style>
