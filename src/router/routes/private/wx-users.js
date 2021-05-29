14167c85d88238584e348f80182cb5dd1a6f6c5a
zhaotoday <729234283@qq.com>
2021-05-29 20:09:42 +0800

    update

--- src/router/routes/private/wx-users.js
+++ src/router/routes/private/wx-users.js
@@ -1,10 +1,10 @@
 export default {
   path: "wx-users",
-  component: () => import("@/views/wx-users"),
+  component: () => import("@/views/wx-users/index.vue"),
   children: [
     {
       path: "list",
-      component: () => import("@/views/wx-users/list"),
+      component: () => import("@/views/wx-users/list/index.vue"),
     },
   ],
 };
