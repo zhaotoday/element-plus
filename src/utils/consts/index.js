70232365f9a914b958912064728255df97ba731a
zhaotoday <729234283@qq.com>
2021-05-29 22:22:41 +0800

    update

--- src/utils/consts/index.js
+++ src/utils/consts/index.js
@@ -4,10 +4,10 @@ import { SidebarMenu } from "@/utils/consts/sidebar-menu";
 const baseUrl = import.meta.env["BASE_URL"];
 
 // 接口地址
-const ApiUrl = import.meta.env["VUE_APP_API_URL"];
+const ApiUrl = import.meta.env["VITE_API_URL"];
 
 // 静态文件地址
-const StaticUrl = import.meta.env["VUE_APP_STATIC_URL"];
+const StaticUrl = import.meta.env["VITE_STATIC_URL"];
 
 // 分页大小
 const PageSize = 10;
