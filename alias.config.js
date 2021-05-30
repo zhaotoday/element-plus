3a6f72e01da1d0f34fc4f2ec24ceafa63e5f02f5
zhaotoday <729234283@qq.com>
2021-05-30 17:10:05 +0800

    配置 WebStorm，识别项目别名

--- alias.config.js
+++ alias.config.js
@@ -1,4 +1,4 @@
-// File -> Settings -> Languages & Frameworks -> JavaScript -> Webpack
+// WebStorm IDE：File -> Settings -> Languages & Frameworks -> JavaScript -> Webpack
 // -> webpack configuration file 选择 alias.config.js
 
 const resolve = (dir) => require("path").join(__dirname, dir);
