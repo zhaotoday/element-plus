36eda9a21ae99f38e86a4ff7ab919f8ef0905198
zhaotoday <729234283@qq.com>
2021-05-31 16:13:45 +0800

    新增 editor 组件

---
+++ element-plus-admin/components/editor/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="c-editor">
+    <div id="editorToolbar" class="c-editor__toolbar"></div>
+    <div id="editorText" class="c-editor__text"></div>
+  </div>
+</template>
+
+<script src="./script.js"></script>
+
+<style lang="scss" src="./style.scss"></style>
