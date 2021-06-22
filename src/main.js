4970d67112dc9b00dc879f4f11eef5f929794d1d
zhaotoday <729234283@qq.com>
2021-06-22 23:32:05 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import "@/assets/styles/global/index.scss";
 import ElementPlus from "element-plus";
 import locale from "element-plus/lib/locale/lang/zh-cn";
-import "element-plus/lib/theme-chalk/index.css";
+// import "element-plus/lib/theme-chalk/index.css";
 import "element-plus-admin/utils/polyfills";
 import { config } from "@/utils/config";
 import { useComponents } from "@/utils/use-components";
