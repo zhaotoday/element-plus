e6f21fc0c573a4b06ada1421839ae4338de51f49
zhaotoday <729234283@qq.com>
2021-06-22 15:04:56 +0800

    update

--- npm/element-plus-admin/composables/use-enums.js
+++ npm/element-plus-admin/composables/use-enums.js
@@ -1,9 +1,11 @@
 import { computed } from "vue";
-import { store } from "@/store";
+import { useStore } from "vuex";
 import { PublicEnumsApi } from "../apis/public/enums";
 
 export const useEnums = () => {
-  const enums = computed(() => store.state["enums"].data);
+  const { dispatch, state } = useStore();
+
+  const enums = computed(() => state["enums"].data);
 
   const getEnums = async () => {
     const { version } = await new PublicEnumsApi().POST({
@@ -11,7 +13,7 @@ export const useEnums = () => {
     });
 
     if (version !== enums.value.config.version) {
-      await store.dispatch("enums/get");
+      await dispatch("enums/get");
     }
   };
 
