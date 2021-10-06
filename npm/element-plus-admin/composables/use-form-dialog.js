d3300b1da66e061b939cb2284d775963505eac8c
zhaojintian <729234283@qq.com>
2021-10-06 10:15:14 +0800

    u

---
+++ npm/element-plus-admin/composables/use-form-dialog.js
@@ -0,0 +1,44 @@
+import { watch } from "vue";
+import { deepCopy } from "jt-helpers";
+
+export const useFormDialog = ({ cDialog, cForm, form, initialModel } = {}) => {
+  watch(
+    () => cDialog.visible,
+    (newVal) => {
+      if (!newVal) {
+        cForm.model = deepCopy(initialModel);
+        form.value && form.value.resetFields();
+      }
+    }
+  );
+
+  const show = ({ id, ...rest } = {}) => {
+    cForm.id = id || 0;
+    cForm.model = deepCopy(id ? rest : initialModel);
+
+    cDialog.visible = true;
+    form.value && form.value.resetFields();
+  };
+
+  const validateField = (field) => {
+    form.value.validateField(field);
+  };
+
+  const validate = () => {
+    return new Promise((resolve, reject) => {
+      form.value.validate(async (valid) => {
+        if (valid) {
+          resolve(cForm);
+        } else {
+          reject();
+        }
+      });
+    });
+  };
+
+  return {
+    show,
+    validate,
+    validateField,
+  };
+};
