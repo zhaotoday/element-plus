ee844c94a18a55936ee5f7d5526841a859c2b407
zhaojintian <729234283@qq.com>
2021-10-14 21:20:37 +0800

    u

---
+++ npm/element-plus-admin/components/office-view/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-dialog
+    custom-class="c-office-view"
+    title="预览"
+    v-model="cDialog.visible"
+    width="900px"
+  >
+    <div v-if="!cDialog.src" class="c-office-view__iframe"></div>
+    <iframe
+      v-else
+      class="c-office-view__iframe"
+      :src="`https://view.officeapps.live.com/op/view.aspx?src=${cDialog.src}`"
+      frameborder="0"
+    ></iframe>
+  </el-dialog>
+</template>
+
+<script src="./script.js"></script>
+
+<style lang="scss" src="./style.scss"></style>
