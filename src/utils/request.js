ee4dd548d74b4ad87550aae300b9c302bfb4322e
zhaojintian <729234283@qq.com>
2022-01-04 10:17:01 +0800

    u

---
+++ src/utils/request.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API,
+  timeout: 5000,
+});
+
+service.interceptors.request.use(
+  (config) => {
+    config.headers["X-Token"] = {};
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+service.interceptors.response.use(
+  (response) => {
+    const res = response.data;
+
+    if (res.code !== 20000) {
+      console.log("error");
+    }
+    if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
+      // re login
+      return Promise.reject(new Error(res.message || "Error"));
+    } else {
+      return res;
+    }
+  },
+  (error) => {
+    console.log("err" + error);
+    return Promise.reject(error);
+  }
+);
+
+export default service;
