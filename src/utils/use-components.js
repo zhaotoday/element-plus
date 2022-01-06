399610f69757e04ad090c43e2f8c254868c506d8
zhaojintian <729234283@qq.com>
2022-01-06 13:27:58 +0800

    u

--- src/utils/use-components.js
+++ src/utils/use-components.js
@@ -1,7 +1,10 @@
 import List from "element-plus-admin/components/list/index.vue";
 import ListImage from "element-plus-admin/components/list-image/index.vue";
+import { Right, Eleme } from "@element-plus/icons";
 
 export const useComponents = (app) => {
   app.component("c-list", List);
   app.component("c-list-image", ListImage);
+  app.component("el-icon-right", Right);
+  app.component("el-icon-eleme", Eleme);
 };
