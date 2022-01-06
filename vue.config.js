e43d00ab84f55c73ad2933f2e10bfa4cf5594900
zhaojintian <729234283@qq.com>
2022-01-06 11:42:40 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   transpileDependencies: ["element-plus-admin"],
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.mjs$/,
+          include: /node_modules/,
+          type: "javascript/auto",
+        },
+      ],
+    },
+  },
 };
