06957a7260ac0df6600bf3ce8f76b52e8cb8d3f7
zhaojintian <729234283@qq.com>
2022-01-07 09:55:51 +0800

    u

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
-  <router-view />
+  <el-config-provider :locale="locale">
+    <router-view />
+  </el-config-provider>
 </template>
 
 <script>
-import { onMounted } from "vue";
+import { defineComponent, onMounted } from "vue";
+import { ElConfigProvider } from "element-plus";
+import zhCn from "element-plus/lib/locale/lang/zh-cn";
 import { useEnums } from "element-plus-admin/composables/use-enums";
 
-export default {
+export default defineComponent({
+  components: {
+    ElConfigProvider,
+  },
   setup() {
     const { getEnums } = useEnums();
 
     onMounted(async () => {
       await getEnums();
     });
+
+    return {
+      locale: zhCn,
+    };
   },
-};
+});
 </script>
