37b59c9aa2e3c9de168c81aa8d88e9b8705ccdd8
zhaojintian <729234283@qq.com>
2022-01-08 19:31:39 +0800

    u

---
+++ npm/element-plus-admin/components/file-viewer/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <el-image-viewer
+    v-if="cImageViewer.visible"
+    :url-list="ids.map((id) => getFileUrl({ id }))"
+    :initial-index="cImageViewer.index"
+    z-index="20000"
+    @close="cImageViewer.visible = false"
+  />
+  <c-office-viewer ref="officeViewer" :service-url="officeViewerServiceUrl" />
+</template>
+
+<script src="./script.js"></script>
