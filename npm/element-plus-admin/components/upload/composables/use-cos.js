c1fbc5d0e23593e0c26d7ec0bb28b232aac643ce
zhaojintian <729234283@qq.com>
2022-01-24 22:37:21 +0800

    u

--- npm/element-plus-admin/components/upload/composables/use-cos.js
+++ npm/element-plus-admin/components/upload/composables/use-cos.js
@@ -1,29 +1,65 @@
 import { filesApi } from "@/apis/admin/files";
-import * as Oss from "ali-oss";
-import {UploadTo} from "../../../enums/upload-to";
+import * as AliCloudOss from "ali-oss";
+import * as TencentCloudCos from "cos-js-sdk-v5";
+import { UploadTo } from "../../../enums/upload-to";
 
 export const useCos = ({ api, uploadTo, region, bucket, onProgress }) => {
   let client = null;
 
   const initialize = async () => {
-    const {
-      Credentials: { AccessKeyId, AccessKeySecret, SecurityToken },
-    } = await api.post({
-      action: "getStsCredential",
-      body: { region, bucket },
-    });
+    switch (uploadTo) {
+      case UploadTo.AliCloudOss:
+        {
+          const {
+            Credentials: { AccessKeyId, AccessKeySecret, SecurityToken },
+          } = await api.post({
+            action: "getStsCredential",
+            body: { region, bucket },
+          });
 
-    switch (uploadTo===UploadTo.AliCloud){
-      
+          client = new AliCloudOss({
+            region,
+            bucket,
+            accessKeyId: AccessKeyId,
+            accessKeySecret: AccessKeySecret,
+            stsToken: SecurityToken,
+          });
+        }
+
+        break;
+
+      case UploadTo.TencentCloudOss:
+        {
+          const {
+            credentials: {
+              tmpSecretId,
+              tmpSecretKey,
+              sessionToken,
+              startTime,
+              expiredTime,
+            },
+          } = await api.post({
+            action: "getStsCredential",
+            body: { region, bucket },
+          });
+          client = new TencentCloudCos({
+            getAuthorization: function (options, callback) {
+              callback({
+                TmpSecretId: tmpSecretId,
+                TmpSecretKey: tmpSecretKey,
+                SecurityToken: sessionToken,
+                // 建议返回服务器时间作为签名的开始时间，避免用户浏览器本地时间偏差过大导致签名错误
+                StartTime: data.startTime, // 时间戳，单位秒，如：1580000000
+                ExpiredTime: data.expiredTime, // 时间戳，单位秒，如：1580000000
+              });
+            },
+          });
+        }
+        break;
+
+      default:
+        break;
     }
-
-    client = new Oss({
-      region,
-      bucket,
-      accessKeyId: AccessKeyId,
-      accessKeySecret: AccessKeySecret,
-      stsToken: SecurityToken,
-    });
   };
 
   const upload = async (file, fileDir) => {
--- npm/element-plus-admin/enums/upload-to.js
+++ npm/element-plus-admin/enums/upload-to.js
@@ -1,5 +1,5 @@
 export const UploadTo = {
   Server: "Server",
-  AliCloud: "AliCloud",
-  TencentCloud: "TencentCloud",
+  AliCloudOss: "AliCloudOss",
+  TencentCloudOss: "TencentCloudOss",
 };
