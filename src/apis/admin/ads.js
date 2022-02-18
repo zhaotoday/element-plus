15dac9c8c9621e7eae380e29ead62256ae31d11b
zhaojintian <729234283@qq.com>
2022-02-18 16:51:47 +0800

    u

---
+++ src/apis/admin/ads.js
@@ -0,0 +1,7 @@
+import { createApi } from "element-plus-admin/utils/create-api";
+import { useAuth } from "element-plus-admin/composables/use-auth";
+
+export const adsApi = createApi({
+  url: "/admin/ads",
+  headers: useAuth().getHeaders(),
+});
--- src/utils/consts/sidebar-menu.js
+++ src/utils/consts/sidebar-menu.js
@@ -1,6 +1,17 @@
 export const SidebarMenu = [
   {
     name: "ad",
+    title: "商品管理",
+    icon: "ad",
+    children: [
+      {
+        title: "商品管理",
+        path: "/ad/ads/list",
+      },
+    ],
+  },
+  {
+    name: "ad",
     title: "广告管理",
     icon: "ad",
     children: [
