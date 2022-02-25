11579cae7b598e31161f6d8e440cf90882f4dea2
zhaojintian <729234283@qq.com>
2022-02-25 09:30:25 +0800

    u

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,4 +1,5 @@
 import { auth } from "element-plus-admin/store/modules/auth";
 import { enums } from "element-plus-admin/store/modules/enums";
+import { items } from "element-plus-admin/store/modules/items";
 
-export default { auth, enums };
+export default { auth, enums, items };
