dec17cc9d1853767ddb2b53a6f6230a1a2f6768c
zhaojintian <729234283@qq.com>
2022-02-25 09:40:37 +0800

    u

--- src/utils/use-components.js
+++ src/utils/use-components.js
@@ -1,11 +1,13 @@
 import List from "element-plus-admin/components/list/index.vue";
 import ListImage from "element-plus-admin/components/list/components/image/index.vue";
+import OrderInput from "element-plus-admin/components/order-input/index.vue";
 import Upload from "element-plus-admin/components/upload/index.vue";
 import { Eleme, Right } from "@element-plus/icons";
 
 export const useComponents = (app) => {
   app.component("c-list", List);
   app.component("c-list-image", ListImage);
+  app.component("c-order-input", OrderInput);
   app.component("c-upload", Upload);
   app.component("el-icon-right", Right);
   app.component("el-icon-eleme", Eleme);
