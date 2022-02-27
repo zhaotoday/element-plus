a2298d13649f3575eb50dfb615b6c5274fabdc69
zhaojintian <729234283@qq.com>
2022-02-27 21:38:28 +0800

    u

--- src/views/users/list/index.vue
+++ src/views/users/list/index.vue
@@ -6,7 +6,6 @@
   >
     <template #operations>
       <el-button type="primary" @click="reRender">刷新</el-button>
-      <el-button type="primary" @click="form.show()">新增</el-button>
     </template>
     <template #filters>
       <el-form
@@ -41,21 +40,8 @@
           <c-order-input :api="usersApi" :row="row" @ok="reRender" />
         </template>
       </el-table-column>
-      <el-table-column label="操作" width="150px">
-        <template #default="{ row }">
-          <el-button type="" size="small" @click="form.show(row)">
-            编辑
-          </el-button>
-          <el-popconfirm title="确定删除吗？" @confirm="del(row)">
-            <template #reference>
-              <el-button type="danger" size="small"> 删除 </el-button>
-            </template>
-          </el-popconfirm>
-        </template>
-      </el-table-column>
     </el-table>
   </c-list>
-  <vc-form ref="form" @render-list="reRender" />
 </template>
 
 <script src="./script.js"></script>
--- src/views/users/list/script.js
+++ src/views/users/list/script.js
@@ -2,16 +2,10 @@ import { useList } from "element-plus-admin/components/list/composables/use-list
 import { useEnums } from "element-plus-admin/composables/use-enums";
 import { ref } from "vue";
 import { usersApi } from "@/apis/admin/users";
-import Form from "./components/form/index.vue";
 import { ElMessage } from "element-plus";
 
 export default {
-  components: {
-    "vc-form": Form,
-  },
   setup() {
-    const form = ref(null);
-
     const { enums } = useEnums();
 
     const { list, currentPage, cFilters, reRender, onPageChange, search } =
@@ -23,20 +17,10 @@ export default {
           },
           rules: {},
         },
-        extraQuery: {
-          order: [["order", "DESC"]],
-        },
       });
-
-    const del = async ({ id }) => {
-      await usersApi.delete({ id });
-      ElMessage.success("删除成功");
-      await reRender();
-    };
 
     return {
       usersApi,
-      form,
       enums,
       list,
       currentPage,
@@ -44,7 +28,6 @@ export default {
       reRender,
       onPageChange,
       search,
-      del,
     };
   },
 };
