cddf99b22df8a0a864ac63766e30dc982658e8b9
zhaojintian <729234283@qq.com>
2022-03-09 20:20:10 +0800

    u

--- src/views/orders/list/components/detail/index.vue
+++ src/views/orders/list/components/detail/index.vue
@@ -1,6 +1,6 @@
 <template>
   <el-dialog title="订单详情" v-model="cDialog.visible" width="550px">
-    <ul class="vc-detail">
+    <ul class="vc-detail c-scrollbar">
       <li>
         <label>订单号</label>
         {{ detail.no }}
@@ -29,9 +29,32 @@
       </li>
       <li>
         <label>购物清单</label>
-        abc
+        <el-table
+          :data="detail.products"
+          size="small"
+          border
+          :show-header="false"
+        >
+          <el-table-column>
+            <template #default="{ row }">
+              {{ row.name }}
+            </template>
+          </el-table-column>
+          <el-table-column width="60">
+            <template #default="{ row }"> ${{ row.price }} </template>
+          </el-table-column>
+          <el-table-column width="60">
+            <template #default="{ row }"> x{{ row.number }} </template>
+          </el-table-column>
+        </el-table>
       </li>
     </ul>
+    <template #footer>
+      <el-button @click="cDialog.visible = false">取消</el-button>
+      <el-button type="primary" @click="cDialog.visible = false">
+        确定
+      </el-button>
+    </template>
   </el-dialog>
 </template>
 
