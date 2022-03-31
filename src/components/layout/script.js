4887012f24db945762d9e6aae180141eeaf55e41
zhaojintian <729234283@qq.com>
2022-03-31 09:58:36 +0800

    update

--- src/components/layout/script.js
+++ src/components/layout/script.js
@@ -1,5 +1,5 @@
-import TheSidebar from "./sidebar/index.vue";
-import TheMain from "./main/index.vue";
+import TheSidebar from "element-plus-admin/components/layout/sidebar/index.vue";
+import TheMain from "element-plus-admin/components/layout/main/index.vue";
 
 export default {
   name: "TheLayout",
