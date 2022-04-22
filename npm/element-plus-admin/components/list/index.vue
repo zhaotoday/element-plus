5dd0f174dad5a18cc3db4c262f768f32a61aec81
zhaojintian <729234283@qq.com>
2022-04-23 01:00:51 +0800

    u

--- npm/element-plus-admin/components/list/index.vue
+++ npm/element-plus-admin/components/list/index.vue
@@ -1,26 +1,28 @@
 <template>
-  <div
-    v-if="showHeader"
-    class="c-list__header"
-    :class="{ 'is-simple': simpleHeader }"
-  >
-    <div class="c-list__operations">
-      <slot name="operations" />
+  <div class="c-list">
+    <div
+      v-if="showHeader"
+      class="c-list__header"
+      :class="{ 'is-simple': simpleHeader }"
+    >
+      <div class="c-list__operations">
+        <slot name="operations" />
+      </div>
+      <div class="c-list__filters">
+        <slot name="filters" />
+      </div>
     </div>
-    <div class="c-list__filters">
-      <slot name="filters" />
+    <slot name="default" />
+    <div v-if="showPagination" class="c-list__pagination">
+      <el-pagination
+        background
+        @current-change="$emit('page-change', $event)"
+        :current-page="currentPage"
+        :page-size="pageSize"
+        layout="total, prev, pager, next, jumper"
+        :total="total"
+      />
     </div>
-  </div>
-  <slot name="default" />
-  <div v-if="showPagination" class="c-list__pagination">
-    <el-pagination
-      background
-      @current-change="$emit('page-change', $event)"
-      :current-page="currentPage"
-      :page-size="pageSize"
-      layout="total, prev, pager, next, jumper"
-      :total="total"
-    />
   </div>
 </template>
 
