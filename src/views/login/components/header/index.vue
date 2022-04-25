25be00456559acafde3c130f35eba488cd2ee7ac
zhaojintian <729234283@qq.com>
2022-04-25 23:04:32 +0800

    u

---
+++ src/views/login/components/header/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="l-header">
+    <div class="c-container">
+      <a class="l-header__logo" href="/"></a>
+      <c-search />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" src="./style.scss"></style>
