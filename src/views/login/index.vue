a8f55152e8a1815c0d244d888e65ff0ec7e71fa7
zhaojintian <729234283@qq.com>
2022-04-26 09:15:33 +0800

    u

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <c-login>
+  <c-login class="v-login">
     <template #header>
       <c-login-header>
         <img src="./images/logo.png" />
@@ -7,62 +7,64 @@
       </c-login-header>
     </template>
     <template #main>
-      <div class="b-login u-shadow u-br2">
-        <div class="b-login__title">管理员登录</div>
-        <el-form
-          class="b-form"
-          ref="form"
-          :model="cForm.model"
-          :rules="cForm.rules"
-        >
-          <el-form-item prop="username">
-            <el-input
-              size="large"
-              v-model="cForm.model.username"
-              @keyup.enter="submit"
-            >
-              <template #prefix>
-                <i
-                  class="c-iconfont c-iconfont--user-filled fs16"
-                  style="margin-left: -6px; margin-right: 6px"
-                ></i>
-              </template>
-            </el-input>
-          </el-form-item>
-          <el-form-item prop="password">
-            <el-input
-              type="password"
-              size="large"
-              autocomplete="new-password"
-              v-model="cForm.model.password"
-              @keyup.enter="submit"
-            >
-              <template #prefix>
-                <i
-                  class="c-iconfont c-iconfont--lock-filled fs16"
-                  style="margin-left: -6px; margin-right: 6px"
-                ></i>
-              </template>
-            </el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-button
-              type="primary"
-              size="large"
-              @click="submit"
-              style="width: 100%; margin-top: 10px"
-            >
-              登录
-            </el-button>
-          </el-form-item>
-        </el-form>
-      </div>
+      <c-login-main>
+        <div class="b-login u-shadow u-br2">
+          <div class="b-login__title">管理员登录</div>
+          <el-form
+            class="b-form"
+            ref="form"
+            :model="cForm.model"
+            :rules="cForm.rules"
+          >
+            <el-form-item prop="username">
+              <el-input
+                size="large"
+                v-model="cForm.model.username"
+                @keyup.enter="submit"
+              >
+                <template #prefix>
+                  <i
+                    class="c-iconfont c-iconfont--user-filled fs16"
+                    style="margin-left: -6px; margin-right: 6px"
+                  ></i>
+                </template>
+              </el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+              <el-input
+                type="password"
+                size="large"
+                autocomplete="new-password"
+                v-model="cForm.model.password"
+                @keyup.enter="submit"
+              >
+                <template #prefix>
+                  <i
+                    class="c-iconfont c-iconfont--lock-filled fs16"
+                    style="margin-left: -6px; margin-right: 6px"
+                  ></i>
+                </template>
+              </el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button
+                type="primary"
+                size="large"
+                @click="submit"
+                style="width: 100%; margin-top: 10px"
+              >
+                登录
+              </el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+      </c-login-main>
     </template>
     <template #footer>
-      <b-footer>
+      <c-login-footer>
         福建省智慧医工联合工程研究中心 &nbsp;&nbsp; fjmec.cn &nbsp;&nbsp;
         闽ICP备2021009415号-1
-      </b-footer>
+      </c-login-footer>
     </template>
   </c-login>
 </template>
--- src/views/login/script.js
+++ src/views/login/script.js
@@ -3,6 +3,7 @@ import { useStore } from "vuex";
 import { useRouter } from "vue-router";
 import { ElMessage } from "element-plus";
 import { useValidators } from "vue-validation";
+import Login from "@/components/login/index.vue";
 import LoginHeader from "@/components/login/components/header/index.vue";
 import LoginMain from "@/components/login/components/main/index.vue";
 import LoginFooter from "@/components/login/components/footer/index.vue";
@@ -10,6 +11,7 @@ import { UserFilled } from "@element-plus/icons";
 
 export default {
   components: {
+    "c-login": Login,
     "c-login-header": LoginHeader,
     "c-login-main": LoginMain,
     "c-login-footer": LoginFooter,
