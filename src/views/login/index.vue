c00f55f41354ae663b35e17dbfdd43483b5d956f
zhaojintian <729234283@qq.com>
2022-04-26 08:09:03 +0800

    u

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -40,27 +40,12 @@
               </template>
             </el-input>
           </el-form-item>
-          <el-form-item prop="password">
-            <el-input
-              type="password"
-              size="large"
-              v-model="cForm.model.password"
-              @keyup.enter="submit"
-            >
-              <template #prefix>
-                <i
-                  class="c-iconfont c-iconfont--lock-filled fs18"
-                  style="margin-left: -8px"
-                ></i>
-              </template>
-            </el-input>
-          </el-form-item>
           <el-form-item>
             <el-button
               type="primary"
               size="large"
               @click="submit"
-              style="width: 100%"
+              style="width: 100%; margin-top: 10px"
             >
               登录
             </el-button>
