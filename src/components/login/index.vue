8af5f73c1dc5c52308f047ab53cb251401c72dd4
zhaojintian <729234283@qq.com>
2022-04-26 08:48:33 +0800

    u

--- src/components/login/index.vue
+++ src/components/login/index.vue
@@ -1,68 +1,9 @@
 <template>
-  <div class="v-login">
-    <b-header>
-      <img src="./images/logo.png" />
-      福州力软信息科技有限公司
-    </b-header>
-    <b-main>
-      <div class="b-login u-shadow u-br2">
-        <div class="b-login__title">管理员登录</div>
-        <el-form
-          class="b-form"
-          ref="form"
-          :model="cForm.model"
-          :rules="cForm.rules"
-        >
-          <el-form-item prop="username">
-            <el-input
-              size="large"
-              v-model="cForm.model.username"
-              @keyup.enter="submit"
-            >
-              <template #prefix>
-                <i
-                  class="c-iconfont c-iconfont--user-filled fs16"
-                  style="margin-left: -6px; margin-right: 6px"
-                ></i>
-              </template>
-            </el-input>
-          </el-form-item>
-          <el-form-item prop="password">
-            <el-input
-              type="password"
-              size="large"
-              autocomplete="new-password"
-              v-model="cForm.model.password"
-              @keyup.enter="submit"
-            >
-              <template #prefix>
-                <i
-                  class="c-iconfont c-iconfont--lock-filled fs16"
-                  style="margin-left: -6px; margin-right: 6px"
-                ></i>
-              </template>
-            </el-input>
-          </el-form-item>
-          <el-form-item>
-            <el-button
-              type="primary"
-              size="large"
-              @click="submit"
-              style="width: 100%; margin-top: 10px"
-            >
-              登录
-            </el-button>
-          </el-form-item>
-        </el-form>
-      </div>
-    </b-main>
-    <b-footer>
-      福建省智慧医工联合工程研究中心 &nbsp;&nbsp; fjmec.cn &nbsp;&nbsp;
-      闽ICP备2021009415号-1
-    </b-footer>
+  <div class="c-login">
+    <slot name="header" />
+    <slot name="main" />
+    <slot name="footer" />
   </div>
 </template>
-
-<script src="./script.js"></script>
 
 <style lang="scss" src="./style.scss"></style>
