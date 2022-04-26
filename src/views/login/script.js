974312a42542bc8b860f164c2356689a6ca55ced
zhaojintian <729234283@qq.com>
2022-04-26 09:27:25 +0800

    u

--- src/views/login/script.js
+++ src/views/login/script.js
@@ -1,14 +1,8 @@
-import { reactive, ref } from "vue";
-import { useStore } from "vuex";
-import { useRouter } from "vue-router";
-import { ElMessage } from "element-plus";
-import { useValidators } from "vue-validation";
 import Login from "@/components/login/index.vue";
 import LoginHeader from "@/components/login/components/header/index.vue";
 import LoginMain from "@/components/login/components/main/index.vue";
 import LoginFooter from "@/components/login/components/footer/index.vue";
 import Form from "./components/form/index.vue";
-import { UserFilled } from "@element-plus/icons";
 
 export default {
   components: {
