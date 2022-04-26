7f4895f6ebdce2e5dd3994fd5f931ff66e9285ea
zhaojintian <729234283@qq.com>
2022-04-26 09:16:33 +0800

    u

--- src/views/login/components/footer/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div class="b-footer">
-    <slot />
-  </div>
-</template>
-
-<style lang="scss" src="./style.scss"></style>
--- src/views/login/components/main/script.js
+++
@@ -1,8 +0,0 @@
-export default {
-  props: {
-    tip: {
-      type: String,
-      default: "实现企业能力统一运营和标准化",
-    },
-  },
-};
