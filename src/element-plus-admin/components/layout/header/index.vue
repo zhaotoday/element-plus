64b5e0329d67e6bcd93ef4336c92bb2ef80568a7
zhaojintian <729234283@qq.com>
2022-07-10 15:59:47 +0800

    u

---
+++ src/element-plus-admin/components/layout/header/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="c-header">
+    <div class="c-header__logo" @click="$router.push('/')">
+      <slot name="logo" />
+    </div>
+    <div class="c-header__user">
+      {{ user.name }}
+      <el-avatar icon="el-icon-right" title="退出" @click="logout" />
+    </div>
+  </div>
+</template>
+
+<script src="./script.js"></script>
+
+<style lang="scss" src="./style.scss"></style>
---
+++ src/element-plus-admin/components/layout/main/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="c-main">
+    <div class="c-main__body">
+      <router-view />
+    </div>
+    <el-breadcrumb class="c-main__breadcrumb" separator="/">
+      <template v-if="menus[0].title">
+        <el-breadcrumb-item>{{ menus[0].title }}</el-breadcrumb-item>
+        <el-breadcrumb-item>{{ menus[1].title }}</el-breadcrumb-item>
+        <el-breadcrumb-item v-if="menus[2].title">
+          {{ menus[2].title }}
+        </el-breadcrumb-item>
+        <el-breadcrumb-item v-if="menus[3].title">
+          {{ menus[3].title }}
+        </el-breadcrumb-item>
+      </template>
+      <template v-else>
+        <el-breadcrumb-item>首页</el-breadcrumb-item>
+      </template>
+    </el-breadcrumb>
+  </div>
+</template>
+
+<script src="./script.js"></script>
+
+<style lang="scss" src="./style.scss"></style>
