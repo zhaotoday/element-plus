5da74a09cbf30dc2838ce3790c188963c8ee7d75
zhaojintian <729234283@qq.com>
2022-07-10 17:05:07 +0800

    u

--- npm/element-plus-admin/components/upload/files/script.js
+++ npm/element-plus-admin/components/upload/files/script.js
@@ -1,6 +1,6 @@
 import { computed, ref, watch } from "vue";
 import { useHelpers } from "@/composables/use-helpers";
-import { CircleCheck, Close, Document } from "@element-plus/icons";
+import { CircleCheck, Close, Document } from "@element-plus/icons-vue";
 import FileViewer from "../../file-viewer/index.vue";
 import { publicFilesApi } from "../../../apis/public/files";
 import { useStore } from "vuex";
