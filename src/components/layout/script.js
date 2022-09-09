76392749183f0d61fd5407c5be30c26630498cd4
zhaojintian <729234283@qq.com>
2022-09-09 15:53:32 +0800

    u

--- src/components/layout/script.js
+++ src/components/layout/script.js
@@ -3,8 +3,7 @@ import TheSidebar from "element-plus-admin/components/layout/sidebar/index.vue";
 import TheMain from "element-plus-admin/components/layout/main/index.vue";
 import { ElMessage } from "element-plus";
 import { useRouter } from "vue-router";
-import { onMounted, ref } from "vue";
-import { useAuth } from "@/composables/use-auth";
+import { useAuth } from "element-plus-admin/composables/use-auth";
 import { getMenus } from "./utils/get-menus";
 
 export default {
@@ -17,22 +16,13 @@ export default {
     const router = useRouter();
     const { user } = useAuth();
 
-    const userName = ref("");
-
-    onMounted(() => {
-      userName.value =
-        user.value && user.value.account
-          ? `${user.value.account}@${user.value.school.name} `
-          : "";
-    });
-
     const logout = async () => {
       await router.push("/logout");
       ElMessage.success("退出成功");
     };
 
     return {
-      userName,
+      user,
       getMenus,
       logout,
     };
