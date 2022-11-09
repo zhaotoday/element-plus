3045ea478f305588919ed35b7337f892e30edfc9
zhaojintian <729234283@qq.com>
2022-11-09 17:06:33 +0800

    update

--- npm/element-plus-admin/components/resource-select/index.vue
+++ npm/element-plus-admin/components/resource-select/index.vue
@@ -17,7 +17,18 @@
           : item[optionLabel]
       "
       :value="item.id"
-    />
+    >
+      <slot
+        name="option"
+        :label="
+          typeof optionLabel === 'function'
+            ? optionLabel(item)
+            : item[optionLabel]
+        "
+        :value="item.id"
+        :item="item"
+      />
+    </el-option>
   </el-select>
 </template>
 
