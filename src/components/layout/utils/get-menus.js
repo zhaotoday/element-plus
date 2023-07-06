707f9eb535198e505c6d2e9c9f91084f90e1c5ba
zhaojintian <729234283@qq.com>
2023-07-06 18:27:40 +0800

    u

--- src/components/layout/utils/get-menus.js
+++ src/components/layout/utils/get-menus.js
@@ -1,63 +1,123 @@
-export const getMenus = () => {
-  return [
-    {
-      name: "product",
-      title: "商品管理",
-      icon: "product-filled",
-      children: [
-        {
-          title: "商品列表",
-          path: "/product/products",
-        },
-        {
-          title: "商品分类",
-          path: "/product/categories",
-        },
-      ],
-    },
-    {
-      name: "ad",
-      title: "广告管理",
-      icon: "ad-filled",
-      children: [
-        {
-          title: "广告列表",
-          path: "/ad/ads",
-        },
-      ],
-    },
-    {
-      name: "order",
-      title: "订单管理",
-      icon: "order-filled",
-      children: [
-        {
-          title: "订单列表",
-          path: "/order/orders",
-        },
-      ],
-    },
-    {
-      name: "user",
-      title: "用户管理",
-      icon: "user-filled",
-      children: [
-        {
-          title: "用户列表",
-          path: "/user/users",
-        },
-      ],
-    },
-    {
-      name: "app",
-      title: "应用管理",
-      icon: "app-filled",
-      children: [
-        {
-          title: "应用升级",
-          path: "/app/app-upgrades",
-        },
-      ],
-    },
-  ];
-};
+export const SidebarMenu = [
+  {
+    name: "introduction",
+    title: "中心介绍",
+    icon: "introduction",
+    children: [
+      {
+        title: "中心简介",
+        path: "/introduction/contents-introduction-summary",
+      },
+      {
+        title: "组织机构",
+        path: "/introduction/contents-introduction-organization",
+      },
+      {
+        title: "学术委员会",
+        path: "/introduction/contents-introduction-committee",
+      },
+    ],
+  },
+  {
+    name: "research",
+    title: "科学研究",
+    icon: "research",
+    children: [
+      {
+        title: "研究方向",
+        path: "/research/research-areas-research-direction",
+      },
+      {
+        title: "科研项目",
+        path: "/research/contents-research-project",
+      },
+      {
+        title: "科研成果",
+        path: "/research/contents-research-fruit",
+      },
+    ],
+  },
+  {
+    name: "team",
+    title: "研究队伍",
+    icon: "team",
+    children: [
+      {
+        title: "工学队伍",
+        path: "/team/team-members-team-ergonomic",
+      },
+      {
+        title: "医学队伍",
+        path: "/team/team-members-team-medicine",
+      },
+      {
+        title: "博硕学生",
+        path: "/team/contents-team-student",
+      },
+    ],
+  },
+  {
+    name: "talent",
+    title: "人才培养",
+    icon: "talent",
+    children: [
+      {
+        title: "教学获奖",
+        path: "/talent/contents-talent-prize",
+      },
+      {
+        title: "学生竞赛获奖",
+        path: "/talent/contents-talent-student-prize",
+      },
+      {
+        title: "优博优硕",
+        path: "/talent/contents-talent-people",
+      },
+    ],
+  },
+  {
+    name: "job",
+    title: "人才招聘",
+    icon: "job",
+    children: [
+      {
+        title: "招聘信息",
+        path: "/job/articles-job-info",
+      },
+      {
+        title: "人才政策",
+        path: "/job/articles-job-policy",
+      },
+    ],
+  },
+  {
+    name: "information",
+    title: "信息发布",
+    icon: "information",
+    children: [
+      {
+        title: "新闻快讯",
+        path: "/information/articles-information-news",
+      },
+      {
+        title: "通知公告",
+        path: "/information/articles-information-notice",
+      },
+    ],
+  },
+  {
+    name: "other",
+    title: "其他",
+    icon: "other",
+    children: [
+      {
+        title: "首页轮播广告",
+        path: "/other/ads",
+      },
+      {
+        title: "联系我们",
+        path: "/other/contents-contact",
+      },
+    ],
+  },
+];
