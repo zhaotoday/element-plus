aadd29e04bc4253bc991bea6047310e22702787c
zhaojintian <729234283@qq.com>
2023-07-08 08:43:22 +0800

    u

--- src/views/jobs/list/components/form/index.vue
+++ src/views/jobs/list/components/form/index.vue
@@ -2,7 +2,7 @@
   <el-dialog
     :title="cForm.id ? '编辑' : '新增'"
     v-model="cDialog.visible"
-    width="550px"
+    width="750px"
   >
     <el-form
       ref="formRef"
@@ -11,18 +11,26 @@
       label-position="right"
       label-width="80px"
     >
-      <el-form-item label="广告标题" prop="title">
-        <el-input v-model.trim="cForm.model.title" />
+      <el-form-item label="职位名称" prop="name">
+        <el-input v-model.trim="cForm.model.name" />
       </el-form-item>
-      <el-form-item label="广告图片" prop="imageFileId">
-        <c-upload
-          :key="`${cForm.id}:imageFileId`"
-          :data="{
-            dir: 'ads',
-          }"
-          v-model:value="cForm.model.imageFileId"
-          @change="validateField('imageFileId')"
-        />
+      <el-form-item label="薪资范围" prop="salaryRange">
+        <el-input v-model.trim="cForm.model.salaryRange" />
+      </el-form-item>
+      <el-form-item label="技能" prop="skills">
+        <el-input v-model.trim="cForm.model.skills" />
+      </el-form-item>
+      <el-form-item label="职位描述" prop="description">
+        <div style="position: relative; z-index: 2">
+          <c-editor
+            style="height: 300px"
+            v-model:value="cForm.model.description"
+            @change="form.validateField('description')"
+          />
+        </div>
+      </el-form-item>
+      <el-form-item label="工作地点" prop="address">
+        <el-input v-model.trim="cForm.model.address" />
       </el-form-item>
     </el-form>
     <template #footer>
