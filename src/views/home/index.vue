d7c47e49dba132a016d1f1678ea411cb51d23ee7
zhaojintian <729234283@qq.com>
2023-07-20 09:58:00 +0800

    u

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,25 +1,3 @@
 <template>
-  <div class="v-home">
-    欢迎使用后台管理系统！
-    <editor v-model:value="value" />
-    {{ value }}
-  </div>
+  <div class="v-home">欢迎使用后台管理系统！</div>
 </template>
-
-<script>
-import Editor from "@/components/editor";
-import { ref } from "vue";
-
-export default {
-  components: {
-    Editor,
-  },
-  setup() {
-    const value = ref("222");
-
-    return {
-      value,
-    };
-  },
-};
-</script>
