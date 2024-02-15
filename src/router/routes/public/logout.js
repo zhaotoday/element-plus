054aef4439b9580bc2c35430f2070482d24f4a45
zhaojintian <729234283@qq.com>
2024-02-15 15:30:45 +0800

    u

--- src/router/routes/public/logout.js
+++ src/router/routes/public/logout.js
@@ -1,7 +1,9 @@
 import { store } from "@/store";
+import { RouterView } from "vue-router";
 
 export const logoutRoute = {
   path: "/logout",
+  component: <RouterView />,
   async beforeEnter(to, from, next) {
     if (store.state.auth.user.token) {
       await store.dispatch("auth/logout");
